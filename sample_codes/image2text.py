69de2d814add2312eafeb5df5f7c0e1af4d3f470
desktop <tmkjny@gmail.com>
2025-01-03 16:04:09 +0900

    init

--- sample_codes/image2text.py
+++ sample_codes/image2text.py
@@ -9,19 +9,6 @@ class ImageText:
         load_dotenv()
         self.client = OpenAI(api_key=os.environ['OpenAIKey'])
 
-    @staticmethod
-    def encode_image_from_url(image_url):
-        try:
-            # 画像をダウンロード
-            response = requests.get(image_url)
-            response.raise_for_status()
-            image_data = response.content
-            # Base64エンコード
-            return base64.b64encode(image_data).decode("utf-8")
-        except requests.exceptions.RequestException as e:
-            print("画像のダウンロード中にエラーが発生しました:", str(e))
-            return None
-
     def image2text(self, image_url):
         """
         画像URLから画像内の文字を抽出する。
@@ -35,24 +22,19 @@ class ImageText:
         if not image_url.strip():
             return "None"  # 空白のみの場合
 
-        # 画像をBase64エンコード
-        encoded_image = self.encode_image_from_url(image_url)
-        if not encoded_image:
-            return "画像のダウンロードまたはエンコードに失敗しました。"
-
         # OpenAI APIへのプロンプト構築
         message = [
             {
                 "role": "user",
                 "content": [
-                    {
-                        "type": "text",
-                        "text": "何がかかれていますか？"
-                    },
+                    {"type": "text", "text": "この画像にかかれている文字を抽出してください。なかった場合なんと言っていそうですか？\n"
+                                             "[Joy, Sadness, Anticipation, Surprise, Anger, Fear, Disgust, Trust]の中で選んでください。テキストのみで出力してください。\n"
+                                             "例: 文字あり: Hello, World!\n"
+                                             "文字なし: Anger"},
                     {
                         "type": "image_url",
                         "image_url": {
-                            "url": f"data:image/png;base64, {encoded_image}",
+                            "url": image_url,
                         },
                     },
                 ],
@@ -61,7 +43,7 @@ class ImageText:
 
         try:
             response = self.client.chat.completions.create(
-                model="gpt-4o",
+                model="gpt-4o-mini",
                 messages=message,
                 temperature=0.2,
             )
@@ -79,15 +61,5 @@ if __name__ == "__main__":
     # インスタンスを作成
     image_text = ImageText()
 
-    # encode_image_from_url メソッドのテスト
-    encoded_image = image_text.encode_image_from_url(image_url)
-
-    if encoded_image:
-        print("Base64エンコード成功:")
-        print(encoded_image[:100] + "...")  # 結果の最初の100文字を表示
-    else:
-        print("画像のダウンロードまたはエンコードに失敗しました。")
-
-    image_url = "https://yt3.ggpht.com/Co2M3WdCbJm7t2q5XZOgBb8a5DQ1PUQpFsF8QkVfWOd-5SRVpb5YyazcczbzQh8yUKOqzocS"
     result = ImageText().image2text(image_url)
     print("結果:", result)
